de757f24a9c14995e5a20c663c002f10e6322230
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-09 18:54:46 -0400

    [implements] ctor for equilateral

--- src/main/kotlin/EquilateralTriangle.kt
+++ src/main/kotlin/EquilateralTriangle.kt
@@ -2,8 +2,8 @@
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/EquilateralTriangle.kt
  * A triangle with equal sides.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-09t17:25Q
+ * When      : 2022-09-09t18:52Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
-class EquilateralTriangle: Triangle{
+class EquilateralTriangle (_name : String): Triangle(_name){
 }
