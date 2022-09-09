3ca313cb3793c53a509b97d52370b1793a189966
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-09 18:42:32 -0400

    [implements] constructors for child classes

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -2,8 +2,8 @@
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/Square.kt
  * A shape with 4 equal sides.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-09t15:46Q
+ * When      : 2022-09-09t18:40Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
-class Square: Shape(""){
+class Square (_name : String): Shape(_name){
 }
