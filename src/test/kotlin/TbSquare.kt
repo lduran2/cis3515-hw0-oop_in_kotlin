8e6ecf7a88fd204be637124fab818512b3475acd
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-10 16:44:32 -0400

    [tests] tb for circle.dimensions, fix TbSquare

--- src/test/kotlin/TbSquare.kt
+++ src/test/kotlin/TbSquare.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/test/kotlin/TbSquare.kt
  * Tests the name and dimension accessors of the dimensions of the square.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-10t16:07Q
+ * When      : 2022-09-10t16:25Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
 class TbSquare {
@@ -16,9 +16,9 @@ class TbSquare {
     @Test
     fun testGetName(){
         /* create a square */
-        val square : Shape = Square(SQUARE_NAME)
+        val shape : Shape = Square(SQUARE_NAME)
         /* check the name */
-        assertEquals(SQUARE_NAME, square.name)
+        assertEquals(SQUARE_NAME, shape.name)
     } /* end fun testGetName() */
 
     /**
@@ -40,8 +40,11 @@ class TbSquare {
      */
     @Test
     fun testDimensions1(){
+        /* create a square */
         val square = Square(SQUARE_NAME)
+        /* set the dimensions */
         square.setDimensions(SQUARE1_LENGTH, SQUARE1_HEIGHT)
+        /* check the dimensions */
         assertEquals(SQUARE1_DIMENSIONS, square.dimensionsToString())
     } /* end fun testDimensions1() */
 
@@ -51,8 +54,13 @@ class TbSquare {
      */
     @Test
     fun testDimensions2(){
-        val square : Square = Square(SQUARE_NAME)
+        /* create a square */
+        val square = Square(SQUARE_NAME)
+        /* set the initial dimensions */
+        square.setDimensions(SQUARE1_LENGTH, SQUARE1_HEIGHT)
+        /* update the dimensions */
         square.setDimensions(SQUARE2_LENGTH, SQUARE2_HEIGHT)
+        /* check the dimensions */
         assertEquals(SQUARE2_DIMENSIONS, square.dimensionsToString())
     } /* end fun testDimensions2() */
 
@@ -64,10 +72,10 @@ const val SQUARE_NAME = "square ABCD"
 const val SQUARE1_LENGTH = 10.0
 const val SQUARE1_HEIGHT = 10.0
 /** expected string representation of test regular square's dimensions */
-const val SQUARE1_DIMENSIONS = "length = 1.0000e+1, height = 1.0000e+1"
+const val SQUARE1_DIMENSIONS = "length = 1.0000e+01, height = 1.0000e+01"
 /** length and height of test irregular square */
 const val SQUARE2_LENGTH = 8.0
 const val SQUARE2_HEIGHT = 12.0
 /** expected string representation of test irregular square's
  * dimensions */
-const val SQUARE2_DIMENSIONS = "length = 8.0000e+0, height = 1.2000e+1"
+const val SQUARE2_DIMENSIONS = "length = 8.0000e+00, height = 1.2000e+01"
