74f06833593982bd1811a99a3c2475ebd07998aa
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-10 19:43:52 -0400

    [implements] circle.dimsToStr to master

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -2,7 +2,7 @@
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/Circle.kt
  * A shape consisting of all equidistant points from a central point.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-09t19:25Q
+ * When      : 2022-09-09t19:43Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
 class Circle (_name : String): Shape(_name){
