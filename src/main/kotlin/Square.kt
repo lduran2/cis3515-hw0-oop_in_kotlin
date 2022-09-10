d3802b053831e74ccc6d1cc9f70373dfde4f8d53
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-10 19:27:15 -0400

    [implements] cleanup square after dimsToString

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,25 +1,27 @@
+import kotlin.reflect.typeOf
+
 /**
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/Square.kt
  * A shape with 4 equal sides.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-09t19:17Q
+ * When      : 2022-09-10t19:06Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
 class Square (_name : String): Shape(_name){
 
     /**
      * The horizontal length of the square.
-     * It must be modified through `setDimensions`.
      * @see #setDimensions
+     * @see #printDimensions
      */
-    var length = 0.0
+    private var length = 0.0
 
     /**
      * The vertical length of the square.
-     * It must be modified through setDimensions.
      * @see #setDimensions
+     * @see #printDimensions
      */
-    var height = 0.0
+    private var height = 0.0
 
     /**
      * Updates the horizontal and vertical lengths of this square.
@@ -31,8 +33,13 @@ class Square (_name : String): Shape(_name){
         this.height = _height
     } /* end fun setDimensions(_length : Double, _height : Double) */
 
+    /**
+     * Represents the dimensions of this square as a string.
+     * @return the string representation
+     */
     override fun dimensionsToString() : String{
-        return "length = ${"%.4e".format(this.length)}, height = ${"%.4e".format(this.height)}"
-    }
+        return ("length = ${"%.4e".format(this.length)}, "
+            + "height = ${"%.4e".format(this.height)}")
+    } /* end fun dimensionsToString() */
 
 } /* end class Square */
