509691e4880ea4fdaa89bd6f208bd49d38a2cf0d
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-11 12:03:52 -0400

    [tests] empty common classes for test benches

---
+++ src/test/kotlin/TbEquilateralTriangle.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+import java.lang.Math.pow
+
+/**
+ * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/test/kotlin/TbEquilateralTriangle.kt
+ * Tests the name and dimension accessors of the dimensions of the equilateral triangle.
+ * By        : Leomar Durán <https://github.com/lduran2>
+ * When      : 2022-09-10t18:15Q
+ * For       : CIS3515/Intro to Mobile Application Development
+ */
+class TbEquilateralTriangle {
+
+} /* end class TbEquilateralTriangle */
---
+++ src/test/kotlin/TbTriangle.kt
@@ -0,0 +1,13 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/test/kotlin/TbTriangle.kt
+ * Tests the name and dimension accessors of the dimensions of the triangle.
+ * By        : Leomar Durán <https://github.com/lduran2>
+ * When      : 2022-09-10t23:14Q
+ * For       : CIS3515/Intro to Mobile Application Development
+ */
+class TbTriangle{
+
+} /* end class TbTriangle */
