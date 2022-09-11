f397a5c972354c7dfe52a440e1a40c1f587d8dd1
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-11 01:54:36 -0400

    [Main] named Shape instances

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,17 @@
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/Main.kt
  * Demonstrates the shape objects.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-11t03:23Q
+ * When      : 2022-09-11t01:52Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
 fun main(args: Array<String>) {
     /* instances of shape classes */
     /* store each in a variable of type Shape */
     val shapes = arrayOf<Shape>(
-        Square(),
-        Circle(),
-        Triangle(),
-        EquilateralTriangle()
+        Square("Square ABCD"),
+        Circle("Circle P"),
+        Triangle("Triangle EFG"),
+        EquilateralTriangle("HIJ")
     )
 
     println("Hello World!")
