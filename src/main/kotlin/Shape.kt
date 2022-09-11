10cbf01a9e466bff3e2bfa135ef93ec36b5b81d0
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-11 13:13:00 -0400

    [Main] empty Shape.scanDimensionsFrom

--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -1,11 +1,14 @@
+import java.io.PrintWriter
+import java.util.*
+
 /**
  * Canonical : https://github.com/lduran2/cis3515-hw0-oop_in_kotlin/blob/dev/src/main/kotlin/Shape.kt
  * Represents a shape taking up an area in two dimensions.
  * By        : Leomar Durán <https://github.com/lduran2>
- * When      : 2022-09-11t11:14Q
+ * When      : 2022-09-11t13:11Q
  * For       : CIS3515/Intro to Mobile Application Development
  */
-abstract class Shape (_name : String): Dimensionable{
+abstract class Shape (_name : String): ScannableDimensionable{
     /** name referring to this shape */
     var name = _name
 
@@ -33,5 +36,13 @@ abstract class Shape (_name : String): Dimensionable{
     open fun dimensionsToString() : String {
         return ""
     } /* end fun dimensionsToString() */
+
+    /**
+     * Prints a prompt to the output stream, then scans in new
+     * dimensions from input.
+     * @param sc : Scanner = from which to scan new dimensions
+     * @param out : PrintWriter = to which to print prompts for input
+     */
+    override fun scanDimensionsFrom(sc : Scanner, out : PrintWriter){}
 
 } /* end class Shape */
